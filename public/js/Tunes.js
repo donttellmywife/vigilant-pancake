f75d8aa2536d395a968675735e8328fb9641f8f5
Vladimir Puchkov <Volodymyr.Puchkov@playtech.com>
2016-10-27 09:02:32 +0300

    horrible first commit with all the dependencies

---
+++ public/js/Tunes.js
@@ -0,0 +1,246 @@
+(function($) {
+    /* ALBUMS */
+    window.Album = Backbone.Model.extend({
+        isFirstTrack: function(index) {
+            return index == 0;
+        },
+
+        isLastTrack: function(index) {
+            return index >= this.get('tracks').length - 1;
+        },
+
+        trackUrlAtIndex: function(index) {
+            if (this.get('tracks').length >= index) {
+                return this.get('tracks')[index].url;
+            }
+
+            return null;
+        }
+    });
+
+
+    window.Albums = Backbone.Collection.extend({
+        model: Album,
+        url: '/albums'
+    });
+
+
+    window.Player = Backbone.Model.extend({
+        defaults: {
+            'currentAlbumIndex': 0,
+            'currentTrackIndex': 0,
+            'state': 'stop'
+        },
+
+
+        initialize: function () {
+            this.playlist = new Playlist();
+        },
+
+
+        play: function () {
+            this.set({'state': 'play'});
+        },
+
+
+        pause: function () {
+            this.set({'state': 'pause'});
+        },
+
+
+        isPlaying: function () {
+            return (this.get('state') === 'play');
+        },
+
+
+        isStopped: function () {
+            return (!this.isPlaying());
+        },
+
+
+        currentAlbum: function () {
+            return this.playlist.at(this.get('currentAlbumIndex'));
+        },
+
+
+        currentTrackUrl: function () {
+            var album = this.currentAlbum();
+
+            return album.trackUrlAtIndex(this.get('currentTrackIndex'));
+        },
+
+
+        nextTrack: function () {
+            var currentTrackIndex = this.get('currentTrackIndex'),
+                currentAlbumIndex = this.get('currentAlbumIndex');
+
+            if (this.currentAlbum().isLastTrack(currentTrackIndex)) {
+                if (this.playlist.isLastAlbum(currentAlbumIndex)) {
+                    this.set({'currentAlbumIndex': 0});
+                    this.set({'currentTrackIndex': 0});
+                } else {
+                    this.set({'currentAlbumIndex': currentAlbumIndex + 1});
+                    this.set({'currentTrackIndex': 0});
+                }
+            } else {
+                this.set({'currentTrackIndex': currentTrackIndex + 1});
+            }
+
+            this.logCurrentAlbumAndTrack();
+        },
+
+
+        prevTrack: function () {
+            var currentTrackIndex = this.get('currentTrackIndex'),
+                currentAlbumIndex = this.get('currentAlbumIndex'),
+                lastModelIndex = 0;
+
+            if (this.currentAlbum().isFirstTrack(currentTrackIndex)) {
+                if (this.playlist.isFirstAlbum(currentAlbumIndex)) {
+                    lastModelIndex = this.playlist.models.length - 1;
+                    this.set({'currentAlbumIndex': lastModelIndex});
+                } else {
+                    this.set({'currentAlbumIndex': currentAlbumIndex - 1});
+                }
+
+                var lastTrackIndex =
+                    this.currentAlbum().get('tracks').length - 1;
+                this.set({'currentTrackIndex': lastTrackIndex});
+            } else {
+                this.set({'currentTrackIndex': currentTrackIndex - 1});
+            }
+
+            this.logCurrentAlbumAndTrack();
+        },
+
+
+        logCurrentAlbumAndTrack: function () {
+            console.log(
+                'Player',
+                this.get('currentAlbumIndex'),
+                ':',
+                this.get('currentTrackIndex'),
+                this
+            );
+        }
+    })
+
+
+    window.library = new Albums();
+    window.player = new Player();
+
+    window.Playlist = Albums.extend({
+        isFirstAlbum: function(index) {
+            return (index == 0);
+        },
+
+        isLastAlbum: function(index) {
+            return (index == (this.models.length - 1));
+        }
+    });
+
+
+    window.AlbumView = Backbone.View.extend({
+        tag: 'li',
+        className: 'album',
+
+        initialize: function() {
+            _.bindAll(this, 'render');
+            this.model.bind('change', this.render);
+
+            this.template = _.template($('#album-template').html());
+        },
+
+
+        render: function() {
+            var renderContent = this.template(this.model.toJSON());
+            $(this.el).html(renderContent);
+
+            return this;
+        }
+    });
+
+
+
+
+
+    /* LIBRARY */
+    window.LibraryAlbumView = AlbumView.extend({
+        events: {
+            'click .queue.add': 'select'
+        },
+
+
+        select: function() {
+            this.collection.trigger('select', this.model);
+            console.log('triggered select', this.model);
+        }
+    });
+
+    window.PlaylistAlbumView = AlbumView.extend({
+
+    });
+
+
+    window.LibraryView = Backbone.View.extend({
+        tagName: 'section',
+        className: 'library',
+
+        initialize: function() {
+            _.bindAll(this, 'render');
+            this.template = _.template($('#library-template').html());
+            this.collection.bind('reset', this.render);
+        },
+
+
+        render: function() {
+            var $albums;
+            var collection = this.collection;
+
+            $(this.el).html(this.template({}));
+            $albums = this.$('.albums');
+            collection.each(function(album) {
+                var view = new LibraryAlbumView({
+                    model: album,
+                    collection: collection
+                });
+
+                $albums.append(view.render().el);
+            });
+
+            return this;
+        }
+    });
+
+
+    window.BackboneTunes = Backbone.Router.extend({
+        routes: {
+            '': 'home',
+            'blank': 'blank'
+        },
+
+        initialize: function() {
+            this.libraryView = new LibraryView({
+                collection: window.library
+            });
+        },
+
+        home: function() {
+            var $container = $('#container');
+            $container.empty();
+            $container.append(this.libraryView.render().el);
+        },
+
+
+        blank: function() {
+            $('#container').empty();
+            $('#container').text('blank');
+        }
+    });
+
+
+    $(function() {
+        window.App = new BackboneTunes();
+        Backbone.history.start();
+    });
+})(jQuery);
